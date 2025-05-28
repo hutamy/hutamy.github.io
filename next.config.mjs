29b2cde9fc87800e3e80a677da33636587e5b8c2
hutamy <hutamy.triesthi@dto.kemkes.go.id>
2025-05-28 19:06:49 +0700

    config for tailwind

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-//   output: 'export',      // Enables static HTML export
-//   assetPrefix: './',     // Makes sure assets are served relative to path
-//   trailingSlash: true,   // Ensures all URLs end with slash (important for GitHub Pages)
+  output: 'export',      // Enables static HTML export
+  assetPrefix: './',     // Makes sure assets are served relative to path
+  trailingSlash: true,   // Ensures all URLs end with slash (important for GitHub Pages)
+  images: {
+    unoptimized: true,  // since Next.js image optimization is not supported in static export
+  },
 };
 
 export default nextConfig;
